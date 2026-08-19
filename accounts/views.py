a7debad4a2dc8129d967a27495c407300f97d1ee
Macho-fresh <chycyber13@gmail.com>
2026-08-19 20:46:34 +0100

    setup auth layer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import User
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        User.objects.create_user(
+            username = username,
+            email = email,
+            password = password
+        )
+
+        return Response({
+            'message': 'User created succefully'
+        }, status = status.HTTP_201_CREATED)
