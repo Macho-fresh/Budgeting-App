0686bf494f14543e8c7ddfb8be05521b371dab6c
Macho-fresh <chycyber13@gmail.com>
2026-08-22 10:38:46 +0100

    added urls

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('create-category/', CreateCategory.as_view()),
+    path('crete-budget/', CreateBudget.as_view()),
+    path('create-transactions/', CreateTransactions.as_view()) 
+]
