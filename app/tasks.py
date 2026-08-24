ef2bfd1a52b9af1b29457477e3db4603365bd2d4
Macho-Fresh <chycyber13@gmail.com>
2026-08-24 04:15:19 -0700

    added celery_beat

---
+++ app/tasks.py
@@ -0,0 +1,42 @@
+from celery import shared_task
+from .models import *
+from django.db import transaction
+from django.utils import timezone
+
+@shared_task
+def weekly_task():
+    with transaction.atomic():
+        weekly_deduct = Transactions.objects.filter(frequency=7)
+        amount = weekly_deduct.amount
+        budget = Budget.amount
+        remaining_money = Budget.remaining_money
+        remaining_money = budget - amount
+        remaining_money.save()
+        print('money deducted!')
+
+
+@shared_task
+def monthly_task():
+    with transaction.atomic():
+        monthly_deduct = Transactions.objects.filter(frequency=30)
+        amount = monthly_deduct.amount
+        budget = Budget.amount
+        remaining_money = Budget.remaining_money
+        remaining_money = budget - amount
+        remaining_money.save()
+        print('money deducted!')
+
+
+@shared_task
+def yearly_task():
+    with transaction.atomic():
+        annually_deduct = Transactions.objects.filter(frequency=365)
+        amount = annually_deduct.amount
+        budget = Budget.amount
+        remaining_money = Budget.remaining_money
+        remaining_money = budget - amount
+        remaining_money.save()
+        print('money deducted!')
+
+# @shared_task
+# def delete_budget():
