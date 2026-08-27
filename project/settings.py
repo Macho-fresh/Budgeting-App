c8501a1b132e8a1a4905c525576d878ae84d527c
Macho-Fresh <chycyber13@gmail.com>
2026-08-27 15:08:35 +0100

    minor container issues

--- project/settings.py
+++ project/settings.py
@@ -151,7 +151,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_TIMEZONE = "UTC"
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
